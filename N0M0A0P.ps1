1e8d95133208531e9c68c6bf592a6053e511aa88
ounistm <35983477+ounistm@users.noreply.github.com>
2022-09-17 00:01:29 +0100

    Create N0M0A0P.ps1

---
+++ N0M0A0P.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+Param(
+    $DownloadURL = " https://nmap.org/dist/nmap-7.93-setup.exe",
+    $DownloadLocation = "$env:APPDATA/Nmap",
+    $InstallLocation = "C:\Tools",
+    $LogLocation = "$DownloadLocation/install.log"
+)
+
+If(! (Test-Path $DownloadLocation) ){
+    New-Item -Path $DownloadLocation -ItemType Directory
+}
+
+If(! (Test-Path $InstallLocation) ){
+    New-Item -Path $InstallLocation -ItemType Directory
+}
+
+$Installer = "$DownloadLocation/nmap-7.93-setup.exe"
+
+Invoke-WebRequest -UseBasicParsing -Uri $DownloadURL -OutFile $Installer
+
+& $Installer /q /log $LogLocation INSTALLLOCATION="$InstallLocation"
