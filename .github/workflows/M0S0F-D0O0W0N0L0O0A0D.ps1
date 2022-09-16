d35a9cd007c7dc9670608eb74cafa1b6e5b9e611
ounistm <35983477+ounistm@users.noreply.github.com>
2022-09-16 19:45:05 +0100

    Create M0S0F-D0O0W0N0L0O0A0D.ps1

---
+++ .github/workflows/M0S0F-D0O0W0N0L0O0A0D.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+Param(
+    $DownloadURL = "https://windows.metasploit.com/metasploitframework-latest.msi",
+    $DownloadLocation = "$env:APPDATA/Metasploit",
+    $InstallLocation = "C:\Tools",
+    $LogLocation = "$DownloadLocation/install.log"
+)
+
+If(! (Test-Path $DownloadLocation) ){
+    New-Item -Path $DownloadLocation -ItemType Directory
+}
+
+If(! (Test-Path $InstallLocation) ){
+    New-Item -Path $InstallLocation -ItemType Directory
+}
+
+$Installer = "$DownloadLocation/metasploit.msi"
+
+Invoke-WebRequest -UseBasicParsing -Uri $DownloadURL -OutFile $Installer
+
+& $Installer /q /log $LogLocation INSTALLLOCATION="$InstallLocation"
